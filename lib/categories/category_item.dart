18c7a60ad7f30c37f6bf77b41e2784d07489ec21
a7md0 <a7m3d699@gmail.com>
2021-02-21 18:19:52 +0300

    Cat items to navigate to new screen

--- lib/categories/category_item.dart
+++ lib/categories/category_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_screen.dart';
+
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
@@ -8,23 +10,38 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline6,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.topRight,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.topRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
+  }
+
+  void selectCategory(BuildContext ctx) {
+    final pageRoute = MaterialPageRoute(
+      builder: (_) {
+        return CategoryMealsScreen();
+      },
+    );
+
+    Navigator.of(ctx).push(pageRoute);
   }
 }
