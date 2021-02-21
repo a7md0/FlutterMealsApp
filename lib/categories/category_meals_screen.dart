d1ef2d69c1edff2c624fe16f971639f2c62a69a8
a7md0 <a7m3d699@gmail.com>
2021-02-21 18:58:48 +0300

    List category meals from dummy data

--- lib/categories/category_meals_screen.dart
+++ lib/categories/category_meals_screen.dart
@@ -1,8 +1,9 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 
 import '../models/category.dart';
+import '../models/meal.dart';
+
+import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static final String routeName = '/category-meals';
@@ -10,13 +11,19 @@ class CategoryMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context).settings.arguments as Category;
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('The receipes for the Category!'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, idx) {
+          return Text(categoryMeals[idx].title);
+        },
       ),
     );
   }
