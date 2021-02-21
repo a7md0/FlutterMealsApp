211c690bf940064dc2e11ad497edb6fd6c908408
a7md0 <a7m3d699@gmail.com>
2021-02-21 17:56:26 +0300

    Wrap CatScreen in scafold

--- lib/categories/categories_screen.dart
+++ lib/categories/categories_screen.dart
@@ -6,15 +6,20 @@ import './category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES.map((cat) {
-        return CategoryItem(cat.title, cat.color);
-      }).toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AhmedMeal'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat.title, cat.color);
+        }).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
